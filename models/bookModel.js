90fc2361fcc19223d366078aed187aec16d8e199
paulj <pauljorhahaha@gmail.com>
2025-01-03 22:21:38 +0800

    CHORE: remove version key

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    publishedDate: { type: Date },
-});
+const bookSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        author: { type: String, required: true },
+        publishedDate: { type: Date },
+    },
+    { versionKey: false }
+);
 
 const Book = mongoose.model("Book", bookSchema);
 
