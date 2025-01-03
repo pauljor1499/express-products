22a0a0507411a8f590e0956cab1b3eb23144b3d2
paulj <pauljorhahaha@gmail.com>
2025-01-03 21:41:32 +0800

    CHORE: add default route

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const { swaggerUi, swaggerSpec } = require("./swagger");
 const booksRoutes = require("./routes/books");
+const connectDB = require("./config/database");
 
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json()); // Parse incoming JSON payloads
+
+// Create a new router instance
+const router = express.Router();
+
+// Define a simple route for the root
+router.get("/", (req, res) => {
+    res.send("Server is up and running!");
+});
+
+// Use the router
+app.use("/", router);
 
 // Swagger setup
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -17,10 +28,7 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/books", booksRoutes);
 
 // Connect to MongoDB
-mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("MongoDB connection error:", err));
+connectDB();
 
 // Start the server
 const PORT = process.env.PORT || 5000;
